c647eb0ab1abb0a687396cf41bc65f49f6ffaa7b
Kifk2f <fianyofriedo2@gmail.com>
2026-03-10 14:07:46 +0000

    Syntaxe de création d'une classe dart

--- dart -- POO/laClasse.dart
+++ dart -- POO/laClasse.dart
@@ -1,9 +1,15 @@
 void main(){
 
 }
+// Une clase est un modèle ou un plan qi définit
+// les attributs(caractéristiques) et les méthodes (comportements) d'un objet.
 
-class Student{
-  //Attributs
+// On peut la considérer comme un module à import 'dart:mirrors';
+
+// ParameterMirrorduquel on crée des objets individuels qui partagent les memes propriétes et fonctionnalités
+
+class Student{ //Commence toujours parune majuscule
+  //Attributs seront les variables
   //Prénom
   //Nom
   //Adresse
@@ -11,10 +17,11 @@ class Student{
   //Orientation
   //Le niveau
 
-  //Constructeur 
+  //Constructeur : la façon pour construire une classe, c'est à dire pour créer des objets à partir de cette classe et est facultatif
+ 
 
 
-  //Méthodes
+  //Méthodes : les fonctions qui seront les comortement de cet objets
       //Participer à un cours
       //Passer un exam
       //Rendre un devoir, ...
