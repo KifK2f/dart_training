1efe7c60e325bcacfd0392ea20216e3fb6ccb7da
Kifk2f <fianyofriedo2@gmail.com>
2026-03-10 14:11:51 +0000

    Syntaxe de création d'un objet dart

--- dart -- POO/lObjet.dart
+++ dart -- POO/lObjet.dart
@@ -1,6 +1,9 @@
 void main(){
   //Créer des objets de la classe Student
   //On peut en créer plusireurs objet pour une même classe
+
+  // L'objet est une nstance ou une matérialisation d'une classe
+// Il représente une entité concrète avec ses propres caractéristiques et son propre comportement, définis par la classe à laquelle il appartient.
   Student mathieu = Student();
   Student marie = Student();
   Student hakim = Student();
