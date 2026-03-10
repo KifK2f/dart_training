442c48fef07c9908429b01c43569d20e9d2759d5
Kifk2f <fianyofriedo2@gmail.com>
2026-03-10 22:14:14 +0000

    Accesseurs et mutateurs Ok

--- dart -- POO/accesseurs_mutateurs.dart
+++ dart -- POO/accesseurs_mutateurs.dart
@@ -1,15 +1,30 @@
 void main(){
   //Créer des objets de la classe Student
   //On peut en créer plusireurs objet pour une même classe
+
+//   Pour contrôler l'accès aux attributs privés, vous
+// pouvez utiliser des accesseurs (getters) et des
+// mutateurs (setters). 
+// Cela permet de définir des règles spécifiques pour lire
+// ou modifier les attributs.
+
+// L’accesseur:
+// type get nomDelAccesseur => AttributPrivé;
+
+// Mutateur:
+// set mutateur(Type paramère) { 
+// //code
+// }
   Student mathieu = Student();
   Student marie = Student();
   Student hakim = Student();
 
-  // print(mathieu.lastname);
-  // print(mathieu.fullName);
-  // print(mathieu.ageString);
+  print(mathieu.lastname);
+  print(mathieu.fullName);
+  print(mathieu.ageString);
+  print(mathieu.lastname);
   print(mathieu.age);
-  mathieu.age = 20;
+  mathieu.age = 12;
   print(mathieu.age);
 
 }
@@ -23,7 +38,7 @@ class Student{
   int _age = 18;
 
   //Accesseurs (getters)
-  String get firstname => _firstName;
+  String get firstname => _firstName; // Quand on utilisera une classe qui sera hors du fichier si on veut accéder à l'attribut _firstName, on utilisera mathieu.firstname et non mathieu._firstName
   String get lastname => _lastName;
   String get fullName => '$_firstName $_lastName';
   int get age => _age;
